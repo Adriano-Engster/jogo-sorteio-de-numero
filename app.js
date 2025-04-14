6af48bca9a744044e80585e64f2aa274ebeb0127
Adriano Engster <110263820+Adriano-Engster@users.noreply.github.com>
2025-04-14 20:14:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo do número secreto';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 450;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 console.log(numeroSecreto); // mostra no console
